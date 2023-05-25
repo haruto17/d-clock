c655612df31104142401dd942b6f19ee6150a33e
haruto17 <s1902272@sendai-nct.jp>
2023-05-25 23:31:16 +0900

    時計だけの表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
 import { useState } from "react";
-import { MantineProvider, Text } from "@mantine/core";
+import { MantineProvider, Text, ColorScheme, ColorSchemeProvider } from "@mantine/core";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 import Clock from "./Clock";
 
 function App() {
-  return (
-    <body>
-      <MantineProvider withGlobalStyles withNormalizeCSS>
-        <Text>Welcome to Mantine!</Text>
-      </MantineProvider>
-      <div>
-        <p>Hello, Tauri!!</p>
-        <Clock />
-      </div>
-    </body>
-  );
+    return (
+        <div>
+            <Clock />
+        </div>
+    );
 }
 
 export default App;
