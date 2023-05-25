11bde2e6fed78b220be7920e6641b04f597e9c86
haruto17 <s1902272@sendai-nct.jp>
2023-05-25 21:39:28 +0900

    create repository

---
+++ src/Clock.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import "./Clock.css";
+
+export function Clock() {
+
+    const [date,setDate] = useState("");
+    const [dow,setDow] = useState("");
+    const [time, setTime] = useState("");
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const now = new Date();
+            const year = now.getFullYear();
+            const dayofweek = now.getDay();
+            let month = String(now.getMonth() + 1);
+            let day = String(now.getDate());
+            let hour = String(now.getHours());
+            let minute = String(now.getMinutes());
+
+            if (month.length === 1) {
+                month = "0" + month;
+            }
+            if (day.length === 1) {
+                day = "0"+day;
+            }
+            if(hour.length === 1) {
+                hour = "0" + hour;
+            }
+            if (minute.length === 1) {
+                minute = "0" + minute;
+            }
+
+            setDate(`${year}/${month}/${day}`);
+            setDow(["Sun","Mon","Tue","Wed","Thu","Fri","Sat"][dayofweek]);
+            setTime(`${hour}:${minute}`);
+        }, 1000);
+        return () => {
+            clearInterval(interval);
+        }
+    });
+
+    return (
+        <>
+            <div>
+                <p>{date}</p>
+            </div>
+            <div>
+                <p>{dow}</p>
+            </div>
+            <div>
+                <p>{time}</p>
+            </div>
+        </>
+    )
+}
+
+export default Clock;
